a89d68419e4b8fef18aa0c27d2bbb477b6319b65
munozlucas <lgaston.munoz@gmail.com>
2022-12-07 08:59:42 -0300

    fix: testing - import user service

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,11 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+jest.mock('./users.service');
 
 describe('UsersController', () => {
   let controller: UsersController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [UsersService],
       controllers: [UsersController],
     }).compile();
 
