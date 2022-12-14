e4e19d4e224e7bd80efff6247c5eb667a13e6771
munozlucas <lgaston.munoz@gmail.com>
2022-12-14 12:01:50 -0300

    feat: test commit

--- jest.config.ts
+++ jest.config.ts
@@ -11,7 +11,7 @@ const config: Config.InitialOptions = {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
   collectCoverageFrom: ['**/*.(t|j)s'],
-  coverageDirectory: '../coverage',
+  coverageDirectory: './coverage',
   testEnvironment: 'node',
 };
 
